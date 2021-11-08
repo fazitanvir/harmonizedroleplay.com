a40e6c0e3c6838b57dcc2cb1d2d840a4a8819711
izMystic <mystic@izmystic.dev>
2021-11-07 19:24:04 -0500

    upload stuff

---
+++ config.php
@@ -0,0 +1,41 @@
+<?php
+    // Website Name
+    $name = "Harmonized Roleplay";
+
+    // Description
+    $desc = "Realistic FiveM Roleplay Community";
+
+    // Theme
+    $theme = "dark"; // dark or light
+
+    // Domain
+    $domain = "http://localhost:5555/"; // 
+
+    // Colors
+    $color = "#ae47ff";
+
+    // Logo
+    $logo = $domain."assets/img/logo.png";
+
+    // About Page
+    $about = "";
+
+    // Navbar Links
+    $link1label = "About";
+    $link1href = $domain."about";
+
+    $link2label = "Apply";
+    $link2href = $domain."apply";
+
+    $link3label = "Status";
+    $link3href = $domain."status";
+
+    $link4label = "Gallery";
+    $link4href = $domain."gallery";
+
+    $link5label = "CAD/MDT";
+    $link5href = $domain."cad";
+
+    // Navbar Array
+	$linkarray = array("link1" => "$link1label#$link1href", "link2" => "$link2label#$link2href", "link3" => "$link3label#$link3href", "link4" => "$link4label#$link4href", "link5" => "$link5label#$link5href");
+?>
---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+  include("config.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title><?php echo $name?> | Home</title>
+
+    <!-- Meta Tags -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="<?php echo $color ?>">
+    <meta property="og:title" content="<?php echo $name ?>">
+	  <meta property="og:description" content="<?php echo $desc ?>">
+	  <meta property="og:image" content="<?php echo $logo ?>">
+    <link rel="icon" type="image/png" href="<?php echo $logo ?>">
+
+    <!-- CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body class="bg-<?php echo $theme ?>">
+    <nav class="navbar navbar-expand-lg navbar-<?php echo $theme ?>" style="background-color: <?php echo $color ?>; box-shadow: 0 0.01vh 1vh 0 #000;">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="<?php echo $domain ?>">
+              <img src="<?php echo $logo ?>" alt="" width="30" height="30" class="d-inline-block align-text-top"> <?php echo $name ?>
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <?php
+                foreach ($linkarray as $key => $link) {
+                  if ($link !== "" && $link !== "#" && $link !== null) {
+                    $piece = explode("#", $link);
+
+                    if ($piece[0] !== $domain) {
+                      echo '<li class="nav-item"> <a class="nav-link" href="'.$piece[1].'">'.$piece[0].'</a> </li>';
+                    }
+                  }
+                }
+              ?>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+    <!-- JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
