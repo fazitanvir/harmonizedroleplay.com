4fb9d64b4ecd07b0f8ee484c251c4a8dc27c1e22
izMystic <mystic@izmystic.dev>
2021-11-08 23:47:18 -0500

    other stuff

--- config.php
+++ config.php
@@ -9,16 +9,30 @@
     $theme = "dark";
 
     // Domain (with trailing slash)
-    $domain = "https://dev.harmonizedroleplay.com/";
+    $domain = "http://localhost:5555/";
 
     // Colors (hex)
     $color = "#ae47ff";
 
-    // Logo
+    // Logo (must be an image ".png, .jpg, .gif, etc")
     $logo = $domain."assets/img/logo.png";
 
     // About Page
     $about = "";
+
+    // Gallery
+    $gallery = [
+		"https://harmonizedroleplay.com/images/logo.png",
+		"https://harmonizedroleplay.com/images/logo.png",
+		"https://harmonizedroleplay.com/images/logo.png",
+		"https://harmonizedroleplay.com/images/logo.png",
+		"https://harmonizedroleplay.com/images/logo.png",
+        "https://harmonizedroleplay.com/images/logo.png",
+		"https://harmonizedroleplay.com/images/logo.png",
+		"https://harmonizedroleplay.com/images/logo.png",
+		"https://harmonizedroleplay.com/images/logo.png",
+		"https://harmonizedroleplay.com/images/logo.png",
+	];
 
     // Navbar Links
     $link1label = "About";
@@ -33,9 +47,12 @@
     $link4label = "Gallery";
     $link4href = $domain."gallery";
 
-    $link5label = "CAD/MDT";
-    $link5href = $domain."cad";
+    $link5label = "Store";
+    $link5href = "https://store.harmonizedroleplay.com/";
+
+    $link6label = "CAD/MDT";
+    $link6href = $domain."cad";
 
     // Navbar Array
-	$linkarray = array("link1" => "$link1label#$link1href", "link2" => "$link2label#$link2href", "link3" => "$link3label#$link3href", "link4" => "$link4label#$link4href", "link5" => "$link5label#$link5href");
+	$linkarray = array("link1" => "$link1label#$link1href", "link2" => "$link2label#$link2href", "link3" => "$link3label#$link3href", "link4" => "$link4label#$link4href", "link5" => "$link5label#$link5href", "link6" => "$link6label#$link6href");
 ?>
