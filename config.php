f8162d9a0afd06fd93b4b7dc50c31ef22e8559ef
izMystic <mystic@izmystic.dev>
2022-06-30 20:29:55 -0400

    update cad link

--- config.php
+++ config.php
@@ -73,7 +73,7 @@
     $link4href = $domain."gallery";
 
     $link5label = "CAD";
-    $link5href = $domain."cad/login.php";
+    $link5href = "https://cad.harmonizedroleplay.com";
 
     $link6label = "Store";
     $link6href = "https://harmonized-roleplay.tebex.io/";
